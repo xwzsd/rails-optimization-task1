150b9b8664766c2fc2b1c174c10245b27a5c1ce3
Vitaliy Tokmyanin <shamik2024@gmail.com>
2020-07-26 16:48:03 +0300

    try_stackprof

---
+++ stackprof.rb
@@ -0,0 +1,9 @@
+# stackprof stackprof.dump
+# stackprof stackprof.dump --method Object#work
+
+require 'stackprof'
+require_relative 'task-1.rb'
+
+StackProf.run(mode: :wall, out: 'stackprof_reports/stackprof.dump', interval: 100) do
+  work
+end
---
+++ stackprof_json.rb
@@ -0,0 +1,12 @@
+# stackprof stackprof.dump
+# stackprof stackprof.dump --method Object#work
+
+require 'json'
+require 'stackprof'
+require_relative 'task-1.rb'
+
+result = StackProf.run(mode: :wall, raw: true) do
+  work
+end
+
+File.write('stackprof_reports/stackprof.json', JSON.generate(result))
