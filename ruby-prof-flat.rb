62243b2c9126a82d4eb635ac111a4560995a51f1
Vitaliy Tokmyanin <shamik2024@gmail.com>
2020-07-31 23:36:08 +0300

    fix_Array#select

--- ruby-prof-flat.rb
+++ ruby-prof-flat.rb
@@ -4,7 +4,7 @@ require_relative 'task-1.rb'
 RubyProf.measure_mode = RubyProf::WALL_TIME
 
 result = RubyProf.profile do
-  work(disable_gc: false)
+  work(disable_gc: true)
 end
 
 printer = RubyProf::FlatPrinter.new(result)
