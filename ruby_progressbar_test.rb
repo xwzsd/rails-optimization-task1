ee823688209d06d787df4f8bd9d295ce1e0c935e
Vitaliy Tokmyanin <shamik2024@gmail.com>
2020-08-01 18:00:11 +0300

    result

---
+++ ruby_progressbar_test.rb
@@ -0,0 +1,9 @@
+require_relative 'task-1'
+require 'ruby-progressbar'
+
+parts = 2
+progressbar = ProgressBar.create(total: parts, format: '%a, %J, %E %B')
+
+progressbar.increment
+work
+progressbar.increment
